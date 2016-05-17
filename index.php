655c135ebd8e5856492af41b83421098f3a5c5fb
amallard <adam.allard@live.com>
2016-05-17 09:11:20 -0700

    Initial commit.:

---
+++ index.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html>
+<main>
+
+    <head>
+        <title>Adam Allard | Software Engineer Portfolio</title>
+
+        <meta name="google-site-verification" content="auMTecNKVG11yeBfCbcJmuv18190dwF7CZQhV-GHf0k" />
+
+        <!-- include bootstrap version 3.3.5 -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <!-- include bootstrap font awesome - (social media icons) -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+        <!-- include Devicons -->
+        <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/master/devicon.min.css">
+
+        <!-- include Google Fonts -->
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arvo">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato">
+
+        <!-- include personal css page -->
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+
+    <body>
+        <div id="home"></div>
+
+        <!-- NAVIGATION -->
+        <div id="nav">
+            <div id="nav-title">
+                <h2>Adam Allard</h2>
+            </div>
+
+            <div id="nav-links">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#port">Portfolio</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- TOP BANNER IMAGE -->
+        <div id="display">
+            <div style="height: 120px;">
+            </div>
+
+            <center>
+                <h1><br>Adam Allard</h1>
+                <h2>Aspiring Software Engineer</h2>
+                <h2 style="padding-bottom: 50px;">Seeking Internship or Entry Level Position</h2>
+            </center>
+        </div>
+
+        <!-- ABOUT -->
+        <div id="about">
+            <center>
+                <h2>His name is Adam Allard. His name is Adam Allard.</h2>
+
+                <br>
+                <p>
+                    Computer programming student waiting for my opportunity to prove my skills in the real world. I am studying Computer Information Systems with a focus on software engineering at Columbia College. If I am able to CLEP enough classes, I will receive my Bachelor's of Science in C.I.S. by December (2016). My focus right now is learning everything that I can learn. I have a strong desire to succeed, and I know in order to do that I must go above and beyond my academic curriculum. Outside of school work, I have taught myself JavaScript, Swift, and PHP. I have just recently started Node.js, and soon hope to dive into NoSQL databases such as MongoDB.
+                </p>
+
+                <div id="skillsIcons">
+                    <i class="devicon-amazonwebservices-original"></i>
+                    <i class="devicon-apple-original"></i>
+                    <i class="devicon-cplusplus-plain"></i>
+                    <i class="devicon-css3-plain"></i>
+                    <i class="devicon-git-plain"></i>
+                    <i class="devicon-html5-plain"></i>
+                    <i class="devicon-java-plain"></i>
+                    <i class="devicon-javascript-plain"></i>
+                    <i class="devicon-jquery-plain"></i>
+                    <i class="devicon-linux-plain"></i>
+                    <i class="devicon-mongodb-plain"></i>
+                    <i class="devicon-mysql-plain"></i>
+                    <i class="devicon-nodejs-plain"></i>
+                    <i class="devicon-php-plain"></i>
+                    <i class="devicon-wordpress-plain"></i>
+                </div>
+
+                <p>
+                    I consider myself an unemployed student, but that's only because my current jobs have nothing to do with my intended career. I have a part time job working in the veteran affairs office of Skagit Valley College in Oak Harbor, WA. On top of that, I am an E-5 in the United States Navy Reserve working as a Yeoman, which is just a fancy word for administrative clerk. Both positions have no affiliation with my programming skills, so describing those duties are better suited for an interview.
+                </p>
+                <br>
+
+            </center>
+        </div>
+
+        <!-- PORT -->
+        <div id="port">
+            <center>
+                <h1>Check out some of my projects and passions</h1>
+
+
+                <div class="proj pro1">
+                    <!-- OUTSIDE DIV = PICTURE -->
+                    <div class="over">
+                        <!-- INSIDE DIV = COLOR OVERLAY -->
+                        <div class="caption">
+                            <!-- CAPTION DIV = CONTENT -->
+                            <h1>College Stuedent</h1>
+                            <p>Studying Computer Information Systems with a focus on Software Engineering at Columbia College.</p>
+                            <p>Maintaining a 3.9 GPA and on track to receive my Bachelor's in Dec '16.</p>
+                            <p>Dean's list recipient for last 6 terms</p>
+                            <p>Member of the SALUTE National Honor's Society</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro2">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Computer Programmer</h1>
+                            <p>I am good at C++ and JavaScript. I have good knowledge of many others.</p>
+                            <p>I love learning new languages and building upon my current ones.</p>
+                            <p>Constantly challenging myself with online code competitions and katas.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro3">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Hacker Enthusiest</h1>
+                            <p>Attended an Amazon Hackathon for the Amazon Echo</p>
+                            <p>Developed 3 applications within the 12 hour window to win an Amazon Echo device.</p>
+                            <p>Applications were developed using JavaScript, Node.js, and AWS.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro4">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>US Navy Sailor</h1>
+                            <p>Spent 4 years active duty fixing computers and other electronic systems on F-18 Aircraft.</p>
+                            <p>Made E-5 and Enlisted Aviation Warfare Specialist in less than 3 years.</p>
+                            <p>Currently in the US Navy Reserves as a Yeoman (Administrative Clerk).</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro5">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Electronics Master</h1>
+                            <p>I love creating and programming new gadgets using Arduino.</p>
+                            <p>Embedded programming in C++ using Microsoft Visual Studio.</p>
+                            <p><a style="color: white;" href="https://www.youtube.com/watch?v=yoTOKh5oWtA&list=PLQ07BhiT-NEn_m-T9ZLSOPfH08cg3xK3M">Check out my YouTube Channel to see all of my Aruidno Projects.</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro6">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Aspiring Teacher</h1>
+                            <p>The best way to master something is by teaching it.</p>
+                            <p>I have been told by many people that I am good at explaining things, so I create YouTube videos explaining various topics, from programming topics to Naval aircraft electronics.</p>
+                            <p><a style="color: white;" href="https://www.youtube.com/channel/UCkkjWh4xWlv9uu1blQohLyA">Check out my YouTube Channel to see all of my Teaching Videos.</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro7">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>iOS Developer</h1>
+                            <p>Developed 2 iPhone applications.</p>
+                            <p><a style="color: white;" href="https://itunes.apple.com/us/app/4-tile/id1070369362?mt=8">4 Tile - Programmed using Swift on XCode7.</a></p>
+                            <p><a style="color: white;" href="https://itunes.apple.com/us/app/julian-clock/id959214765?mt=8">Julian Clock - Created in Objective-C using XCode5.</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro8">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Cheesehead</h1>
+                            <p>Raised in Wisconsin, I am a diehard Green Bay Packers fan.</p>
+                            <p>Life gets rough during football season living in Seahawks territory.</p>
+                            <p>I took this photo last year when the Packers beat the Seahawks.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro9">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Washington Resident</h1>
+                            <p>I live in beautiful Oak Harbor.</p>
+                            <p>Oak Harbor is a small town, so trying to find software engineering positions is difficult. </p>
+                            <p>All I ask is for an opportunity to prove myself.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro10">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Sole Proprietor</h1>
+                            <p>I started my own website development and design company here in Oak Harbor.</p>
+                            <p>I have created Wordpress CMS websites as well as custom built websites like the one you're on.</p>
+                            <p><a style="color: white;" href="pragmaticways.com" target="_blank">Pragmatic Ways</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro11">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Adventurist</h1>
+                            <p>When I am not programming or studying, I love going hiking or justs being outdoors with my family.</p>
+                            <p>I have an amazing wife, beautiful daughter, and a son on the way.</p>
+                            <p>We also have two cats, because programmers love cats.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="proj pro12">
+                    <div class="over">
+                        <div class="caption">
+                            <h1>Your Next Employee</h1>
+                            <p><a style="color: white;" href="/Resume_Adam_Allard.pdf" target="_blank">Get my resume</a></p>
+                            <p>Imagine me working at your company.</p>
+                            <p>Now imagine your company's productivity increase by 5 Million percent. Call me today.</p>
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+
+        <!-- CLEAR FLOAT: LEFT -->
+        <div style="clear: both;"></div>
+
+        <!-- CONTACT -->
+        <div id="contact">
+            <center>
+                <h2>How to hire me</h2>
+                <h3><i class="fa fa-file-pdf-o fa"></i><a href="/Resume_Adam_Allard.pdf" target="_blank">&nbsp;&nbsp;Get My Resume</a></h3>
+                <h3><i class="fa fa-phone fa"></i>&nbsp;&nbsp;(559) 904-0914</h3>
+                <h3><i class="fa fa-envelope fa"></i>&nbsp;&nbsp;adam.allard@live.com</h3>
+
+                <div id="social">
+                    <a href="https://github.com/Amallard" target="_blank"><i class="fa fa-github fa" style="color: #000;"></i></a>
+                    <a href="https://www.youtube.com/channel/UCkkjWh4xWlv9uu1blQohLyA" target="_blank"><i class="fa fa-youtube fa" style="color: #e52d27;"></i></a>
+                    <a href="http://codepen.io/Amallard/" target="_blank"><i class="fa fa-codepen fa" style="color: #000;"></i></a>
+                    <a href="http://www.freecodecamp.com/amallard" target="_blank"><i class="fa fa-fire fa" style="color: #006400;"></i></a>
+                    <a href="https://www.linkedin.com/in/adam-allard-28b542b1" target="_blank"><i class="fa fa-linkedin fa" style="color:
+                            #0077b5;"></i></a>
+                    <a href="https://www.facebook.com/adam.allard.338" target="_blank"><i class="fa fa-facebook fa" style="color:
+                            #3b5998;"></i></a>
+                    <a href="https://twitter.com/RealAdamAllard" target="_blank"><i class="fa fa-twitter fa" style="color: #55acee;"></i></a>
+                </div>
+            </center>
+        </div>
+
+        <!-- FOOTER -->
+        <div id="footer">
+            <div id="foot-links">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#port">Portfolio</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</main>
+
+<!-- include jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+<!-- SCROLL TO ANCHOR LINK -->
+<script>
+    function scroll_if_anchor(href) {
+        href = typeof (href) == "string" ? href : $(this).attr("href");
+
+        var fromTop = 50;
+
+        // If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
+        // Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+        if (href.indexOf("#") == 0) {
+            var $target = $(href);
+
+            // Older browser without pushState might flicker here, as they momentarily
+            // jump to the wrong position (IE < 10)
+            if ($target.length) {
+                $('html, body').animate({
+                    scrollTop: $target.offset().top - fromTop
+                });
+                if (history && "pushState" in history) {
+                    history.pushState({}, document.title, window.location.pathname + href);
+                    return false;
+                }
+            }
+        }
+    }
+
+    // When our page loads, check to see if it contains and anchor
+    scroll_if_anchor(window.location.hash);
+
+    // Intercept all anchor clicks
+    $("body").on("click", "a", scroll_if_anchor);
+</script>
+
+<!-- CAPTION HOVER ON PORT -->
+<script>
+    $(document).ready(function () {
+        $(".caption").fadeOut();
+        $(".proj").mouseenter(function () {
+            var proj = $(this).find('.proj')
+                , caption = $(this).find('div');
+            caption.width(proj.width());
+            caption.height(proj.height());
+            caption.fadeIn();
+        }).mouseleave(function () {
+            var proj = $(this).find('.proj')
+                , caption = $(this).find('div');
+            caption.width(proj.width());
+            caption.height(proj.height());
+            caption.fadeOut();
+        });
+    });
+</script>
+
+</html>
